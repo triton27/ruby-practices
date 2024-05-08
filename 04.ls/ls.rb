9d08039f4f8961a8b589fb5aa530b8d2850e08d7
triton27 <na54heart@gmail.com>
2024-05-08 19:28:02 +0900

    lsコマンドを作る5：引数にファイルやディレクトリを指定可能にする(歓迎要件)

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,7 +6,7 @@ require_relative './long_format_methods'
 def main
   option = ARGV.getopts('a', 'r', 'l')
 
-  file_and_directory_names = get_file_and_directory_names(option)
+  file_and_directory_names = get_file_and_directory_names(option, ARGV)
 
   if option['l']
     LongFormatMethods.output(file_and_directory_names)
@@ -15,14 +15,17 @@ def main
   end
 end
 
-def get_file_and_directory_names(option)
-  file_and_directory_names = Dir.glob('*') unless option['a'] || option['r']
-  file_and_directory_names = Dir.entries('.').sort if option['a']
-
-  if option['r']
-    tmp = file_and_directory_names.nil? ? Dir.glob('*') : file_and_directory_names
-    file_and_directory_names = []
-    file_and_directory_names << tmp.pop while tmp.size.positive?
+def get_file_and_directory_names(option, argv)
+  if argv.empty?
+    file_and_directory_names = Dir.glob('*')
+    file_and_directory_names = Dir.entries('.').sort if option['a']
+    if option['r']
+      tmp = file_and_directory_names
+      file_and_directory_names = []
+      file_and_directory_names << tmp.pop while tmp.size.positive?
+    end
+  else
+    file_and_directory_names = argv
   end
 
   file_and_directory_names
