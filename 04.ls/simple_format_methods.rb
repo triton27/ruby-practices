55970fb79c8d38a6eb8679b38a1b8ab89bb2c8e4
triton27 <na54heart@gmail.com>
2024-05-08 22:43:40 +0900

    lsコマンドを作る5：半角英数字以外のファイル名（ひらがなや漢字など）を持つファイルがあっても表示が崩れないようにする(歓迎要件)

--- 04.ls/simple_format_methods.rb
+++ 04.ls/simple_format_methods.rb
@@ -2,13 +2,8 @@ MAXIMAM_COLUMNS = 3
 module SimpleFormatMethods
   def output(file_and_directory_names)
     formatted_list = push_elem_to_three_lists(file_and_directory_names)
-    output_list = nil_into_list(formatted_list)
+    output_list = pad_with_nil(formatted_list)
     puts(output_list.transpose.map { |row| row.join(' ') })
-  end
-
-  def justify_left(list)
-    max_maxlength = list.map(&:size).max
-    list.map { |item| item.ljust(max_maxlength) }
   end
 
   # ターミナルの幅に関わらず横に最大３列を維持するため
@@ -21,13 +16,38 @@ module SimpleFormatMethods
     tmp
   end
 
-  def nil_into_list(formatted_list)
-    # 各配列の要素数を揃えるために、要素数が足りないリストにnilを入れる
-    max_elem = formatted_list.max_by(&:size).size
-    formatted_list.each do |elem|
-      elem << nil while elem.size < max_elem
+  def justify_left(strings)
+    widths = calculate_widths(strings)
+    max_width = widths.max
+    justified_words = []
+    widths.each_with_index do |width, idx|
+      justified_words << strings[idx] + ' ' * (max_width - width)
+    end
+    justified_words
+  end
+
+  def calculate_widths(strings)
+    strings.map { |string| calculate_width(string) }
+  end
+
+  def calculate_width(string)
+    string.each_char.reduce(0) do |sum, char|
+      sum + (zenkaku?(char) ? 2 : 1)
     end
   end
 
-  module_function :output, :justify_left, :push_elem_to_three_lists, :nil_into_list
+  def zenkaku?(char)
+    byte_size = char.encode('UTF-8').bytesize
+    # バイト数が1なら半角、それ以外は全角と判定
+    byte_size > 1
+  end
+
+  def pad_with_nil(strings)
+    # 各配列の要素数を揃えるために、要素数が足りないリストにnilを入れる
+    max_length = strings.map(&:size).max
+    strings.map { |string| string + [nil] * (max_length - string.size) }
+  end
+
+  module_function :output, :justify_left, :push_elem_to_three_lists,
+                  :pad_with_nil, :calculate_widths, :calculate_width, :zenkaku?
 end
