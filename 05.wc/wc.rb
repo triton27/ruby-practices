22eb760afdc00b9c6ee0d945198e701589097d97
triton27 <na54heart@gmail.com>
2024-05-27 18:40:23 +0900

    wcコマンドを作る： 自作のls -lコマンドと自作のwcコマンドをつなげた場合を対応

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,42 +3,17 @@
 require 'optparse'
 
 def main
-  total_rows, total_words, total_bytes, files_len, options = process_files
-
-  # 引数にファイルを2件以上指定した場合、各要素の合計値を出力する
-  print_totals(total_rows, total_words, total_bytes, options) if files_len > 1
-end
-
-def process_files
-  total_rows  = 0
-  total_words = 0
-  total_bytes = 0
-
-  options, files = parse_input
-  files_len = files.length
-
-  files.each do |file|
-    file_content = read_file_content(file)
-    rows, words, bytes = calculate_file_metrics(file_content, options)
-    if files_len > 1
-      if options.values.none?
-        total_rows  += rows
-        total_words += words
-        total_bytes += bytes
-      else
-        options[:has_lines_option] ? total_rows  += rows  : rows  == nil
-        options[:has_words_option] ? total_words += words : words == nil
-        options[:has_bytes_option] ? total_bytes += bytes : bytes == nil
-      end
-    end
-
-    print_files(rows, words, bytes, file, options)
+  if $stdin.tty?
+    options, files = parse_entries
+    total_rows, total_words, total_bytes = calculate_from_files(files, options)
+    print_totals(total_rows, total_words, total_bytes, options, true) if files.length > 1
+  else
+    total_rows, total_words, total_bytes = calculate_from_stdin
+    print_totals(total_rows, total_words, total_bytes, [], false)
   end
-
-  [total_rows, total_words, total_bytes, files_len, options]
 end
 
-def parse_input
+def parse_entries
   options = inspect_options(ARGV.select { |x| x.start_with?('-') })
   files = ARGV.reject { |arg| arg.start_with?('-') }
 
@@ -53,6 +28,21 @@ def inspect_options(options)
   { has_lines_option:, has_words_option:, has_bytes_option: }
 end
 
+def calculate_from_files(files, options)
+  total_rows = 0
+  total_words = 0
+  total_bytes = 0
+
+  files.each do |file|
+    file_content = read_file_content(file)
+    rows, words, bytes = calculate_metrics(file_content, options)
+    total_rows, total_words, total_bytes = update_totals(rows, words, bytes, total_rows, total_words, total_bytes)
+    print_files(rows, words, bytes, file, options)
+  end
+
+  [total_rows, total_words, total_bytes]
+end
+
 def read_file_content(file)
   File.read(file)
 rescue Errno::ENOENT
@@ -63,51 +53,69 @@ rescue StandardError => e
   puts "Error: #{e.message}"
 end
 
-def calculate_file_metrics(file_content, options)
-  if options.values.none?
-    rows  = file_content.count("\n")
-    words = count_words(file_content)
-    bytes = file_content.bytesize
+def calculate_from_stdin
+  total_rows = 0
+  total_words = 0
+  total_bytes = 0
+
+  $stdin.to_a.each do |input|
+    rows, words, bytes = calculate_metrics(input, [])
+    total_rows, total_words, total_bytes = update_totals(rows, words, bytes, total_rows, total_words, total_bytes)
+  end
+
+  [total_rows, total_words, total_bytes]
+end
+
+def calculate_metrics(content, options)
+  if options.empty? || options.values.none?
+    rows  = content.count("\n")
+    words = content.split.count
+    bytes = content.bytesize
   else
-    rows  = file_content.count("\n")  if options[:has_lines_option]
-    words = count_words(file_content) if options[:has_words_option]
-    bytes = file_content.bytesize     if options[:has_bytes_option]
+    rows  = content.count("\n")  if options[:has_lines_option]
+    words = content.split.count  if options[:has_words_option]
+    bytes = content.bytesize     if options[:has_bytes_option]
   end
 
   [rows, words, bytes]
 end
 
-def count_words(text)
-  words = text.scan(/\b\w+\b/)
-  words.count
+def update_totals(rows, words, bytes, total_rows, total_words, total_bytes)
+  total_rows += rows if rows
+  total_words += words if words
+  total_bytes += bytes if bytes
+
+  [total_rows, total_words, total_bytes]
 end
 
 def custom_padding(options)
-  padding_lines = 0
-  words_padding = 0
-  padding_bytes = 0
+  padding_lines = 8
+  padding_words = 7
+  padding_bytes = 7
 
-  options[:has_lines_option] ? padding_lines = 8 : padding_lines
-  options[:has_words_option] ? words_padding = 7 : words_padding
-  options[:has_bytes_option] ? padding_bytes = 7 : padding_bytes
+  unless options.empty? || options.values.none?
+    padding_lines = options[:has_lines_option] ? padding_lines : 0
+    padding_words = options[:has_words_option] ? padding_words : 0
+    padding_bytes = options[:has_bytes_option] ? padding_bytes : 0
+  end
 
-  [padding_lines, words_padding, padding_bytes]
+  [padding_lines, padding_words, padding_bytes]
 end
 
 def print_files(rows, words, bytes, file, options)
-  padding_lines, words_padding, padding_bytes = custom_padding(options)
-
-  puts "#{rows.to_s.rjust(padding_lines)} #{words.to_s.rjust(words_padding)} #{bytes.to_s.rjust(padding_bytes)} #{file}"
+  padding_lines, padding_words, padding_bytes = custom_padding(options)
+  puts "#{rows.to_s.rjust(padding_lines)} #{words.to_s.rjust(padding_words)} #{bytes.to_s.rjust(padding_bytes)} #{file}"
 end
 
-def print_totals(total_rows, total_words, total_bytes, options)
+def print_totals(total_rows, total_words, total_bytes, options, should_output_total)
   total_rows  = nil if total_rows.zero?
   total_words = nil if total_words.zero?
   total_bytes = nil if total_bytes.zero?
 
-  padding_lines, words_padding, padding_bytes = custom_padding(options)
+  padding_lines, padding_words, padding_bytes = custom_padding(options)
+  total = should_output_total ? 'total' : ''
 
-  puts "#{total_rows.to_s.rjust(padding_lines)} #{total_words.to_s.rjust(words_padding)} #{total_bytes.to_s.rjust(padding_bytes)} total"
+  puts "#{total_rows.to_s.rjust(padding_lines)} #{total_words.to_s.rjust(padding_words)} #{total_bytes.to_s.rjust(padding_bytes)} #{total}"
 end
 
-main
+main if __FILE__ == $PROGRAM_NAME
